0237aeef73e22640d6113195b1c440365b093e0c
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-04-19 20:21:00 -0300

    funcion de empleado

--- src/clases/Empleado.php
+++ src/clases/Empleado.php
@@ -7,4 +7,8 @@ class Empleado extends Usuario {
     public function __construct(int $id, string $nombre, string $email, string $password, string $rol) {
         parent::__construct($id, $nombre, $email, $password, $rol);
     }
+
+    public function mostrarPerfil(): string {
+        return "Empleado: " . $this->getNombre() . " - Email: " . $this->getEmail() . "<br>Gracias por ser parte de nuestro equipo.";
+    }
 }
